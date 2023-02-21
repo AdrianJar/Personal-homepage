aff4f411ef6044ef99046404ded3403e248a9dfa
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-02-22 00:12:06 +0100

    added component and styled it

---
+++ src/components/Hero/index.js
@@ -0,0 +1,10 @@
+import { StyledHero } from "./styled";
+
+const Hero = () => {
+
+    return (
+        <StyledHero>123</StyledHero>
+    );
+}
+
+export default Hero;
---
+++ src/components/Hero/styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+
+export const StyledHero = styled.div`
+    position: absolute;
+    max-width: 1089px;
+    height: 384px;
+    margin: 119px auto;
+`;
