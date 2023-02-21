113980c32e193230ef79cc237e7b501bf89b7b62
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-02-22 00:11:02 +0100

    Added some components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container } from "./components/Container";
+import Hero from "./components/Hero";
+import { StyledTile } from "./components/Tile/styled";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <StyledTile transparent>
+        <Hero />
+      </StyledTile >
+    </Container>
   );
 }
 
