8f0a2f400cc1cf33b201b9968f882a1583f2183d
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-02-22 00:11:26 +0100

    Added theme file

---
+++ src/theme.js
@@ -0,0 +1,17 @@
+export const theme = {
+    color: {
+        black: "#000",
+        white: "#fff",
+        teal: "#008080",
+        silver: "#cccccc",
+        gallery: "#eee",
+        forest_green: "#1a7a1d",
+        pomegranate: "#ee2b2b",
+        alto: "#ddd",
+        mercury: "#E5E5E5"
+    },
+
+    breakpoint: {
+        mobileMax: 767,
+    }
+};
