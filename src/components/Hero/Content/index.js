9519dbdb970f9ab2faaf22c60fc3c9804312e269
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-02-22 01:57:15 +0100

    added component Content

---
+++ src/components/Hero/Content/index.js
@@ -0,0 +1,16 @@
+import { Header, Paragraph, ThisIs, Wrapper } from "./styled";
+
+const Content = () => {
+    return (
+        <Wrapper>
+            <ThisIs>THIS IS</ThisIs>
+            <Header>Adrian Jaroszuk</Header>
+            <Paragraph>
+                🧑🏽‍🦱💻 I'm passionate Frontend Developer in love with React.
+                currently looking for new job opportunities.
+            </Paragraph>
+        </Wrapper>
+    );
+};
+
+export default Content;
