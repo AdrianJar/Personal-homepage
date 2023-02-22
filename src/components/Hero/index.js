27cfcfe120aac0880ad2abfe341ede65ba07adb5
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-02-22 15:21:11 +0100

    Added transparency to component and darkmode comp

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,13 +1,15 @@
 import { Tile } from "../../common/Tile/styled";
 import { Image } from "./Image/styled";
-import me from "../../image/me.jpeg"
+import me from "../../../src/images/me.jpeg"
 import Content from "./Content";
+import DarkMode from "./DrakMode";
 
 const Hero = () => {
     return (
-        <Tile>
+        <Tile transparent>
             <Image src={me} alt="Adrian Jaroszuk"/>
             <Content/>
+            <DarkMode/>
         </Tile>
     );
 }
