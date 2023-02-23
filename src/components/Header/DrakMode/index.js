f5165657adda0f8a198ebd9d587b2473f7cbe768
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-02-23 11:29:59 +0100

    Added darkMode component

---
+++ src/components/Header/DrakMode/index.js
@@ -0,0 +1,13 @@
+import { DMButton, DMStatus, DMWrapper } from "./styled";
+
+const DarkMode = () => {
+    return (
+        <DMWrapper>
+            <DMStatus>dark mode on</DMStatus>
+            <DMButton>SUN</DMButton>
+        </DMWrapper>
+
+    );
+};
+
+export default DarkMode;
