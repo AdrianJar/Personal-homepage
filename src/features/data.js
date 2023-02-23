ee15dcc37d303728a1f5d9b66660e0a47cbc77ea
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-02-24 00:00:55 +0100

    Created myData file

---
+++ src/features/data.js
@@ -0,0 +1,8 @@
+export const myData = {
+
+    name: "Adrian Jaroszuk",
+    description: "🧑🏽‍🦱💻 I'm passionate Frontend Developer in love with React. Currently looking for first job opportunities.",
+    email: "adrianjaroszuk571@gmail.com",
+
+    
+};
