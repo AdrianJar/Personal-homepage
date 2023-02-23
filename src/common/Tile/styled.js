f0b96f5420492ab7e5dcdbca8c2c51bd1f40cb19
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-02-23 11:27:43 +0100

    Added position relative to styled comp

--- src/common/Tile/styled.js
+++ src/common/Tile/styled.js
@@ -1,10 +1,11 @@
 import styled, { css } from "styled-components";
 
 export const Tile = styled.article`
-max-width: 1216px;
-background-color: ${({ theme }) => theme.color.white};
-display: grid;
-grid-template-columns: auto 1fr;
+    max-width: 1216px;
+    background-color: ${({ theme }) => theme.color.white};
+    display: flex;
+    flex-direction: row;
+    position: relative;
 
     ${({ transparent }) => transparent && css`
         background-color: transparent;
