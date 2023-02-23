bb99947372e18358674c955dd3568c96f423b4f1
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-02-23 11:25:48 +0100

    Changed Hero comp for Header comp

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { Container } from "./components/Container";
-import Hero from "./components/Hero";
+import Header from "./components/Header";
 
 
 function App() {
   return (
     <Container>
-      <Hero>12412313421
-      </Hero>
+      <Header>
+      </Header>
     </Container>
   );
 }
