1e5deea44b2f082c9611b070c3ffc9c52b8aca1b
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-02-24 23:53:40 +0100

    Added Portfolio component

---
+++ src/features/PersonalHomepage/Portfolio/index.js
@@ -0,0 +1,13 @@
+import { Wrapper, StyledImage } from "./styled";
+import github from "../../../images/github.svg"
+
+const Portfolio = () => {
+    return(
+        <Wrapper>
+            <StyledImage src={github} alt="github" />
+            1211ko2ko1k2
+        </Wrapper>        
+    );
+};
+
+export default Portfolio
