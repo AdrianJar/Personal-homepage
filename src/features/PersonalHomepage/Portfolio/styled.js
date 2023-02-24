ef4ea084a402f58522978593e3ab0f08d26f9c95
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-02-24 23:53:57 +0100

    Added styled comp to Portfolio

---
+++ src/features/PersonalHomepage/Portfolio/styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 72px;
+`;
+
+export const StyledImage = styled.img`
+    width: 40px;
+    height: 40px;
+`;
