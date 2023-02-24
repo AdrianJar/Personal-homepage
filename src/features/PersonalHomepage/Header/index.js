bda9312e63cec76e332e1f40f630decd1d37a048
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-02-24 01:57:30 +0100

    moved component and added alt name to image

---
+++ src/features/PersonalHomepage/Header/index.js
@@ -0,0 +1,18 @@
+import { Image } from "./Image/styled";
+import me from "../../../../src/images/me.jpeg"
+import Content from "./Content";
+import DarkMode from "./DrakMode";
+import { myData } from "../data";
+import { StyledSection } from "./styled";
+
+const Header = () => {
+    return (
+        <StyledSection>
+            <Image src={me} alt={myData.name} />
+            <Content />
+            <DarkMode />
+        </StyledSection>
+    );
+}
+
+export default Header;
