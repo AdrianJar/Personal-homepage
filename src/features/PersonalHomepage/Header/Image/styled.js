e9d6e5e4955f65cad6fc02fa449c107331276d4c
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-02-24 23:30:37 +0100

    added object fit and obcjet position to image

--- src/features/PersonalHomepage/Header/Image/styled.js
+++ src/features/PersonalHomepage/Header/Image/styled.js
@@ -1,7 +1,9 @@
 import styled from "styled-components";
 
 export const Image = styled.img`
-height: 398px;
-width: 398px;
-border-radius: 50%;
+    height: 398px;
+    width: 398px;
+    border-radius: 50%;
+    object-fit: cover;
+    object-position: top;
 `;
