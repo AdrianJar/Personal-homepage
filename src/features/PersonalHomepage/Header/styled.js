7a581ba884d427c9c0c272d329bb60911d4dbf3d
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-02-24 01:59:05 +0100

    Created new styled component

---
+++ src/features/PersonalHomepage/Header/styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledSection = styled.div`
+    max-width: 1216px;
+    background-color: transparent;
+    display: flex;
+    flex-direction: row;
+    position: relative;
+    margin-bottom: 72px;
+`;
