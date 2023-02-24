d42bc989627cebb6038fc1babc5b335f48392c57
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-02-24 23:27:30 +0100

    Added color and background color, word break ...

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -13,7 +13,10 @@ html {
 
 body {
     font-family: 'Inter', sans-serif;
-    background: ${({ theme }) => theme.color.mercury};
+    background: ${({ theme }) => theme.color.background};
+    color: ${({theme}) => theme.color.text};
     margin: 120px 0 110px;
+    word-break: break-word;
+    letter-spacing: 0.05em;
 }
 `;
