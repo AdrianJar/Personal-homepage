a86b509d72b7a6ecc3d2cc4ffe27a235dc4898aa
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-02-24 23:53:26 +0100

    Added portfolio component to PersonalHomepage

--- src/features/PersonalHomepage/index.js
+++ src/features/PersonalHomepage/index.js
@@ -2,6 +2,7 @@ import { Container } from "../../common/Container"
 import Header from "./Header"
 import Skills from "./Skills";
 import { myData } from "./data";
+import Portfolio from "./Portfolio";
 
 const PersonalHomepage = () => {
     return (
@@ -9,6 +10,7 @@ const PersonalHomepage = () => {
             <Header />
             <Skills title="My skillset includes 🛠️" skills={myData.currentSkills} />
             <Skills title="What I want to learn next 🚀" skills={myData.skillsToLearn} />
+            <Portfolio />
         </Container>
     );
 };
