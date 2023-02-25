58608184f4ba16fdfc1558a15bf6403d60382858
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-02-25 22:42:51 +0100

    removed one styled component and moved  to other1

--- src/features/PersonalHomepage/Header/Content/styled.js
+++ src/features/PersonalHomepage/Header/Content/styled.js
@@ -11,28 +11,10 @@ export const Image = styled.img`
     border-radius: 50%;
 `;
 
-export const ThisIs = styled.caption`
-    display: flex;
-    margin-top: 64px;
-    color: ${({ theme }) => theme.color.textSecondary};
-    font-weight: 700;
-    font-size: 12px;
-    line-height: 15.6px;
-`;
-
 export const Header = styled.h1`
     margin-top: 12px;
     font-weight: 900;
     font-size: 38px;
     line-height: 46px;
     color: ${({ theme }) => theme.color.text};
-`;
-
-export const Paragraph = styled.p`
-    max-width: 633px;
-    margin: 35px 0;
-    font-size: 20px;
-    font-weight: 400;
-    line-height: 28px;
-    color: ${({ theme }) => theme.color.textSecondary};
 `;
