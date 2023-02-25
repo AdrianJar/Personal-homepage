fe394a0396fdacd0664e3e86ec59ffc8c2e20e4a
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-02-25 22:44:30 +0100

    created demo component

---
+++ src/features/PersonalHomepage/Portfolio/Project/Demo/index.js
@@ -0,0 +1,12 @@
+import { Paragraph } from "../../../../../common/styled";
+
+const Demo = (demo) => {
+
+    return (
+        <Paragraph project>Demo: {demo}</Paragraph>
+    )
+}
+
+
+
+export default Demo
