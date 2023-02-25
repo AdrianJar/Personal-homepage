5406cc5230f51abc96844e1d55cabfe4aa77359d
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-02-25 22:45:06 +0100

    removed Title comp. from styled to common styled

--- src/features/PersonalHomepage/Skills/styled.js
+++ src/features/PersonalHomepage/Skills/styled.js
@@ -8,33 +8,20 @@ export const Wrapper = styled.div`
     box-shadow: 0px 16px 58px 0px #090A3308, 0px -2px 50px 0px #090A3305;
 `;
 
-export const Title = styled.h2`
-    font-weight: 900;
-    font-style: normal;
-    font-size: 30px;
-    line-height: 36px;
-    letter-spacing: 0.05em;
-    margin: 0px;
-    color: ${({theme})=>theme.color.text};
-    width: 100%;
-    border-bottom: 1px solid rgba(209, 213, 218, 0.3);
-    padding-bottom: 15px;
-`;
-
 export const ItemList = styled.ul`
     display: grid;
     grid-template-columns: 1fr 1fr 1fr;
     grid-gap: 8px 105px;
     margin: 0;
-    padding: 32px 0;
-    font-size: 18px;
-    letter-spacing: 0.05em;
-    line-height: 1.4;
-
+    padding: 32px 0 0 0;
 `;
 
 export const Item = styled.li`
     display: flex;
     align-items: baseline;
     gap: 16px;
+    color: ${({theme})=>theme.color.textSecondary};
+    font-size: 18px;
+    letter-spacing: 0.05em;
+    line-height: 1.4;
 `;
