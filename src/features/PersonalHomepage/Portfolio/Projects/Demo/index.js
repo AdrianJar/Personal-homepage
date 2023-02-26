14d88b4002df168ab6eaf0d96ad371ac00635971
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-02-26 23:54:21 +0100

    .

---
+++ src/features/PersonalHomepage/Portfolio/Projects/Demo/index.js
@@ -0,0 +1,13 @@
+import { Paragraph } from "../../../../../common/styled";
+
+const Demo = ({ demo }) => {
+
+    return (
+        <Paragraph project>Demo: 
+        {demo}
+        
+        </Paragraph>
+    )
+}
+
+export default Demo
