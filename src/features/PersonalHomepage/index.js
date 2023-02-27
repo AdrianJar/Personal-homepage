9e35797219b4f74b9a92b22e3cd46df4b46fd049
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-02-27 22:33:48 +0100

    imported and implemented Footer to app

--- src/features/PersonalHomepage/index.js
+++ src/features/PersonalHomepage/index.js
@@ -3,6 +3,7 @@ import Header from "./Header"
 import Skills from "./Skills";
 import { myData } from "./data";
 import Portfolio from "./Portfolio";
+import Footer from "./Footer";
 
 const PersonalHomepage = () => {
     return (
@@ -11,6 +12,7 @@ const PersonalHomepage = () => {
             <Skills title="My skillset includes 🛠️" skills={myData.currentSkills} />
             <Skills title="What I want to learn next 🚀" skills={myData.skillsToLearn} />
             <Portfolio />
+            <Footer />
         </Container>
     );
 };
