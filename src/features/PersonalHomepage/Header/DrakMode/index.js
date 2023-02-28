f628dd00a6334b3ea7bc105e51952cf5a82ca21f
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-03-01 00:26:36 +0100

    addec icon to button

--- src/features/PersonalHomepage/Header/DrakMode/index.js
+++ src/features/PersonalHomepage/Header/DrakMode/index.js
@@ -1,10 +1,10 @@
-import { DMButton, DMStatus, DMWrapper } from "./styled";
+import { DMButton, DMIcon, DMStatus, DMWrapper } from "./styled";
 
 const DarkMode = () => {
     return (
         <DMWrapper>
             <DMStatus>dark mode on</DMStatus>
-            <DMButton></DMButton>
+            <DMButton><DMIcon /></DMButton>
         </DMWrapper>
 
     );
