5f2bb4b86816417c61bd45d5ba8d628eb1642728
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-03-04 17:41:20 +0100

    removed background color

--- src/features/PersonalHomepage/PersonalHomepage/Header/styled.js
+++ src/features/PersonalHomepage/PersonalHomepage/Header/styled.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 export const StyledSection = styled.div`
     max-width: 1216px;
-    background-color: transparent;
     display: flex;
     flex-direction: row;
     position: relative;
