96cff7b51289029bd42331f334ae50b1a4e00813
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-03-04 17:38:58 +0100

    updated theme colors

--- src/common/ThemeSwitch/styled.js
+++ src/common/ThemeSwitch/styled.js
@@ -13,30 +13,31 @@ export const Button = styled.button`
     border: none;
     display: flex;
     align-items: center;
-    background-color: white;
+    background: none;
+    color: inherit;
     cursor: pointer;
 `;
 
-export const Status = styled.p`
+export const Status = styled.span`
     font-weight: 700;
     font-size: 12px;
     line-height: 1.3;
     text-transform: uppercase;
-    color: ${({ theme }) => theme.color.textSecondary};;
+    color: ${({theme})=> theme.colors.site.text};
 `;
 
-export const Box = styled.div`
+export const Box = styled.span`
+    background-color: ${({theme}) => theme.colors.themeSwitch.background};
     display: flex;
     align-items: center;
     width: 48px;
     height: 26px;
-    background-color: silver;
-    border: 1px solid #6E7E91;
+    border: 1px solid;
     border-radius: 12px;
     margin-left: 12px;
 `;
 
-export const IconWrapper = styled.div`
+export const IconWrapper = styled.span`
     background: currentColor;
     padding: 3px;
     border-radius: 50%;
@@ -50,5 +51,5 @@ export const IconWrapper = styled.div`
 
 export const Icon = styled(Toggle)`
     margin: 3px;
-    color: ${({ theme }) => theme.color.textSecondary};
+    color: ${({theme}) => theme.colors.themeSwitch.icon};
 `;
