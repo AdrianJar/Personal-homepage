6f01a02ff7d56a98d64eccf575138f36e3e1e509
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-03-07 20:24:02 +0100

    Added replaceAll to name and changed name of link to Project demo and Github repository

--- src/features/PersonalHomepage/PersonalHomepage/Portfolio/Projects/Repositories/index.js
+++ src/features/PersonalHomepage/PersonalHomepage/Portfolio/Projects/Repositories/index.js
@@ -1,26 +1,24 @@
 import { Description, Link, Links, LinksRow, LinksValue, List, Name, Tile } from "./styled";
 
 export const Repositories = ({ repositories }) => (
-
-
     <List>
         {repositories.map(({ id, name, description, homepage, html_url }) => (
             <Tile key={id}>
-                <Name>{name}</Name>
+                <Name>{name.replaceAll("-", " ")}</Name>
                 <Description>{description}</Description>
                 <Links>
                     {!!homepage && (
                         <LinksRow>
                             <dt>Demo:</dt>
                             <LinksValue>
-                                <Link target={"_blank"} href={homepage}>{homepage}</Link>
+                                <Link target={"_blank"} href={homepage}>Project demo</Link>
                             </LinksValue>
                         </LinksRow>
                     )}
                     <LinksRow>
                         <dt>Code:</dt>
                         <LinksValue>
-                            <Link target={"_blank"} href={html_url}>{html_url}</Link>
+                            <Link target={"_blank"} href={html_url}>Github repository</Link>
                         </LinksValue>
                     </LinksRow>
                 </Links>
